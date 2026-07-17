5adc143aa859736e43454005e4549d1e9f21d20e
Cornellius Galuh <trisetyogaluh@gmail.com>
2026-07-18 00:57:42 +0700

    Setup Project

---
+++ orchestrator/src/test/java/com/shipping/orchestrator/OrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipping.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ shipment-service/src/test/java/com/shipping/shipment_service/ShipmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipping.shipment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
