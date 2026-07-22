d75a6eab60102333d72592178a64bf0bb68ff8e0
yerky syabana <yerkysabana1994@gmail.com>
2026-07-23 02:53:35 +0700

    perbaikan

--- tracking-service/src/main/java/com/shipping/tracking_service/controller/TrackingController.java
+++ tracking-service/src/main/java/com/shipping/tracking_service/controller/TrackingController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/tracking")
 public class TrackingController {
 
